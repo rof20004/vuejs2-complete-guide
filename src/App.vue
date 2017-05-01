2452bde9aed748cc8a277227750095f06969cbad
Rodolfo do Nascimento Azevedo <rof20004@gmail.com>
2017-05-01 18:18:49 -0400

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,24 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <h1>Filters & Mixins</h1>
-
+                <h1>Http</h1>
+                <div class="form-group">
+                  <label>Username</label>
+                  <input type="text" class="form-control" v-model="user.username">
+                </div>
+                <div class="form-group">
+                  <label>Mail</label>
+                  <input type="text" class="form-control" v-model="user.email">
+                </div>
+                <button class="btn btn-primary" @click="submit">Submit</button>
+                <hr>
+                <button class="btn btn-primary" @click="fetchData">Get Data</button>
+                <br><br>
+                <ul class="list-group">
+                  <li class="list-group-item" v-for="u in users">
+                    {{ u.username }} - {{ u.email }}
+                  </li>
+                </ul>
             </div>
         </div>
     </div>
@@ -11,9 +27,38 @@
 
 <script>
     export default {
+      data () {
+        return {
+          user: {
+            username: '',
+            email: ''
+          },
+          users: []
+        }
+      },
+      methods: {
+        submit () {
+          this.$http.post('', this.user)
+                    .then(response => console.log(response))
+                    .catch(error => console.log(error))
+        },
+        fetchData () {
+          this.$http.get('')
+                    .then(response => {
+                      return response.json()
+                    })
+                    .then(data => {
+                      const resultArray = []
+                      for (let key in data) {
+                        resultArray.push(data[key])
+                      }
+                      this.users = resultArray
+                    })
+                    .catch(error => console.log(error))
+        }
+      }
     }
 </script>
 
 <style>
-
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import Vue from 'vue'
+import VueResource from 'vue-resource'
 import App from './App.vue'
+
+Vue.use(VueResource)
+
+Vue.http.options.root = 'https://vuejs-http-83855.firebaseio.com/data.json'
 
 new Vue({
   el: '#app',
