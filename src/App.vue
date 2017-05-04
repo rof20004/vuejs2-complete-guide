60f95daeabcd5d9bf9859cb09c31e92f12b17661
Rodolfo do Nascimento Azevedo <rof20004@gmail.com>
2017-05-03 23:17:00 -0400

    Finished lesson about vue-resource

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,8 @@
                 </div>
                 <button class="btn btn-primary" @click="submit">Submit</button>
                 <hr>
+                <input type="text" class="form-control" v-model="node">
+                <br><br>
                 <button class="btn btn-primary" @click="fetchData">Get Data</button>
                 <br><br>
                 <ul class="list-group">
@@ -33,17 +35,21 @@
             username: '',
             email: ''
           },
-          users: []
+          users: [],
+          resource: {},
+          node: 'data'
         }
       },
       methods: {
         submit () {
-          this.$http.post('', this.user)
+          /*this.$http.post('data.json', this.user)
                     .then(response => console.log(response))
-                    .catch(error => console.log(error))
+                    .catch(error => console.log(error))*/
+          /*this.resource.save({}, this.user)*/
+          this.resource.saveAlt(this.user)
         },
         fetchData () {
-          this.$http.get('')
+          /*this.$http.get('data.json')
                     .then(response => {
                       return response.json()
                     })
@@ -54,8 +60,27 @@
                       }
                       this.users = resultArray
                     })
-                    .catch(error => console.log(error))
+                    .catch(error => console.log(error))*/
+          this.resource.getData({ node: this.node })
+              .then(response => {
+                return response.json()
+              })
+              .then(data => {
+                const resultArray = []
+                for (let key in data) {
+                  resultArray.push(data[key])
+                }
+                this.users = resultArray
+              })
+              .catch(error => console.log(error))
         }
+      },
+      created () {
+        const customActions = {
+          saveAlt: { method: 'POST', url: 'alternative.json' },
+          getData: { method: 'GET' }
+        }
+        this.resource = this.$resource('{node}.json', {}, customActions)
       }
     }
 </script>
--- src/main.js
+++ src/main.js
@@ -4,7 +4,16 @@ import App from './App.vue'
 
 Vue.use(VueResource)
 
-Vue.http.options.root = 'https://vuejs-http-83855.firebaseio.com/data.json'
+Vue.http.options.root = 'https://vuejs-http-83855.firebaseio.com'
+Vue.http.interceptors.push((request, next) => {
+  console.log(request)
+  if (request.method == 'POST') {
+    request.method = 'PUT'
+  }
+  next(response => {
+    response.json = () => { return { messages: response.body } }
+  })
+})
 
 new Vue({
   el: '#app',
