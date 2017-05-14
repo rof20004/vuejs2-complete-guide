9d3e87730e29237874f4677655cf47ce20510743
Rodolfo do Nascimento Azevedo <rof20004@gmail.com>
2017-05-13 22:39:51 -0400

    Finished The Stock Trader project

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
 import { routes } from './routes'
 import store from './store/store'
 
 Vue.use(VueRouter)
+Vue.use(VueResource)
+
+Vue.http.options.root = 'https://vuejs-stock-trader-8a7de.firebaseio.com/'
 
 Vue.filter('currency', value => {
   return 'R$ ' + value.toLocaleString()
